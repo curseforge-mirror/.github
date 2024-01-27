92cc521c9984ac24909952c6107d149c2902c197
srhinos <6531393+srhinos@users.noreply.github.com>
2024-01-27 15:08:00 -0500

    Handle Repo Pagination

--- github_api.py
+++ github_api.py
@@ -26,18 +26,20 @@ async def api_request(url, method="GET", data=None, headers=None):
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 async def get_repos(org_name):
-    url = f"https://api.github.com/orgs/{org_name}/repos"
+    url = f"https://api.github.com/orgs/{org_name}/repos?per_page=100&page="
     try:
         logger.info("Fetching repositories from GitHub.")
-        repos = await api_request(url)
         valid_repos = []
-        for repo in repos:
-            if repo and not repo.get("private") and not repo.get("archived"):
-                repo["languages"] = await get_languages(repo["languages_url"])
-                if not repo["languages"]:
-                    continue
-                valid_repos.append(repo)
-        return valid_repos
+        for page in range(1, 5):
+            repos = await api_request(f"{url}{page}")
+            if not repos:
+                return valid_repos
+            for repo in repos:
+                if repo and not repo.get("private") and not repo.get("archived"):
+                    repo["languages"] = await get_languages(repo["languages_url"])
+                    if not repo["languages"]:
+                        continue
+                    valid_repos.append(repo)
     except Exception as e:
         logger.error(f"Error fetching repositories: {e}")
         return []
@@ -78,7 +80,7 @@ async def update_readme(org_name, repo_name, content, sha, new_content):
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 async def create_release(org_name, repo_name, release_name, body):
     url = f"https://api.github.com/repos/{org_name}/{repo_name}/releases"
-    releases = api_request(url)
+    releases = await api_request(url)
     latest_version = len(releases) + 1
     try:
         logger.info(f"Creating release {release_name} for {repo_name}")
